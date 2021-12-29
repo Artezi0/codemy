93a6b13d5dc5f3b2c6a7730eaf8b37484ec79fe4
Artezi0 <mrezi2007@gmail.com>
2021-12-29 17:36:12 +0700

    Commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
-var banner = document.getElementById('banner')
+/* Variables */
+var banner = document.getElementById ('banner')
 
+/* Banner show/hide function */
 window.onscroll = function() {
     var top = document.body.scrollTop + document.documentElement.scrollTop == 0
     banner.style.opacity = top ? '1' : '0'
     banner.style.transition = 'all .3s'
 }
 
+
+/* 
+Enable for smooth scroll
+This shi still got some bugs
+
 luxy.init()
+*/
 
 
 
